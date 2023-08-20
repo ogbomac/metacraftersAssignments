1d67c07bc1e71ecfa0e8ee6b48ca467ec8608a86
ogbomac <142545280+ogbomac@users.noreply.github.com>
2023-08-20 22:15:51 +0100

    Create omaCoin.sol

---
+++ ethProofBeginner/omaCoin.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "OMACOIN";
+    string public tokenAbbrv = "OMC";
+
+    uint public totalSupply = 0;
+
+    // mapping variable here
+    mapping (address => uint) public balances;
+
+    // mint function
+    function mint(address _person, uint _amount) public {
+        totalSupply += _amount;
+        balances[_person] += _amount;
+    }
+
+    // burn function
+    function burn(address _person, uint _amount) public {
+
+        require(balances[_person] >= _amount, "The person should have the number of OMACOINs you want to destroy");
+
+        totalSupply -= _amount;
+        balances[_person] -= _amount;
+    }
+
+}
